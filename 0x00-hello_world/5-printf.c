ec12daeeb370e8e4dcdb25363cf8737e7711d661
APIYOJENNIFER <apiyojennifer4@gmail.com>
2022-10-13 23:33:45 +0000

    Print text using printf followed by newline

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - use printf() to print out text
+ *
+ * Return: 0 if success, otherwise non-positive integer
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
