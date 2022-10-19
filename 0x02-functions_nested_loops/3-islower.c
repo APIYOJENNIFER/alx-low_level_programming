92e39a767cedb6d22a8aa16ea6f89749dca39d42
APIYOJENNIFER <apiyojennifer4@gmail.com>
2022-10-19 09:17:35 +0000

    Check for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+
+/**
+ * _islower - check for lowercase character
+ * @c: value of character
+ *
+ * Return: 1 if true, 0 if false
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0. Print _putchar, followed by a new line
 1. Print the alphabet, in lowercase, followed by a new line
 2. Print 10 times the alphabet, in lowercase, followed by a new line
+3. Check for lowercase character
