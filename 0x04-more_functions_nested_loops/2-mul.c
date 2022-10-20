75e928224eed7040d5a00bb128f031173d7b69e3
APIYOJENNIFER <apiyojennifer4@gmail.com>
2022-10-20 18:43:19 +0000

    Multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiply two values
+ * @a: hold first value
+ * @b: hold second value
+ *
+ * Return: 0 Always
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0. Check for uppercase character
 1. Check for a digit (0 - 9)
+2. Multiply two integers	
