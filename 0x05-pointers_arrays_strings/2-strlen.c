95080d00a759e69bae6c10369571d3ec429f424b
APIYOJENNIFER <apiyojennifer4@gmail.com>
2022-10-24 12:24:18 +0000

    Return length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+/**
+ * _strlen - calculate length of a string
+ * @s: pointer to string
+ * Return: 0 Always
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0. Take a pointer to an int as parameter and updates the value it points to to 98
 1. Swap the value of two integers
+2. Return length of a string
