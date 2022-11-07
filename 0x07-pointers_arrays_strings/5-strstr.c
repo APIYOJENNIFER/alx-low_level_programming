79e6f9d68ae5ce7e60f69392e1a957c5344e5a80
APIYOJENNIFER <apiyojennifer4@gmail.com>
2022-11-07 13:19:43 +0000

    locate a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strstr - locate a substring
+ * @haystack: hold string
+ * @needle: hold substring
+ *
+ * Return: pointer to beginning of located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && haystack[i] == needle[0])
+		{
+			if (haystack[i + j] == needle[j])
+			{
+				j++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@
 2. Locate a character in a string
 3. Get the length of a prefix substring
 4. Search a string for any of a set of bytes
+5. Locate a substring
