258b9844634f8d3a89c0140df113bcc9cdc6f032
APIYOJENNIFER <apiyojennifer4@gmail.com>
2022-11-07 14:53:41 +0000

    return the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * factorial - return factorial of a number
+ * @n: hold given number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		i = n * factorial(n - 1);
+	}
+	return (i);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,4 @@
 0. Print a string, followed by a new line
 1. Print a string in reverse
-2. Return the length of a string.
+2. Return the length of a string
+3. Return the factorial of a given number
