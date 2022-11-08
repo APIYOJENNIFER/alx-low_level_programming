70546b91961d22a97f3873688a980505839fa098
APIYOJENNIFER <apiyojennifer4@gmail.com>
2022-11-08 03:56:32 +0000

    multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply two numbers
+ * @argc: arguments count
+ * @argv: pointer to arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+
+	printf("%d\n", i * j);
+
+	return (0);
+
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 0. Print program name
 1. Print the number of arguments passed to a program
 2. Print all arguments received by a program
+3. Multiply two numbers
