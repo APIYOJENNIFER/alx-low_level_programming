e56b0395b3d0dd5fdf63f6aff8af6e90c51e036a
APIYOJENNIFER <apiyojennifer4@gmail.com>
2022-11-16 18:29:18 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog object
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
